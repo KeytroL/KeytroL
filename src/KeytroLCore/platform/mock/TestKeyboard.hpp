91db8dfd2bf33527ccfba23eaabb9d867c31742e
Alain Martin <amartin@keytrol.org>
2015-09-03 23:31:26 +0200

    Introduce KL::testing::TestKeyboard

---
+++ src/KeytroLCore/platform/mock/TestKeyboard.hpp
@@ -0,0 +1,35 @@
+// KeytroL
+// Copyright (C) 2015 Alain Martin
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "KL/Keyboard.hpp"
+
+
+namespace KL
+{
+namespace testing
+{
+
+class TestKeyboard
+{
+public:
+    static TestKeyboard & instance();
+
+private:
+    TestKeyboard() = default;
+
+    PrivateSignal<Keyboard::KeyCode> mKeyPressed;
+    PrivateSignal<Keyboard::KeyCode> mKeyReleased;
+};
+
+} // namespace testing
+} // namespace KL
