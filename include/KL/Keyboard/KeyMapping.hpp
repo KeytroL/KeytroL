fb3c7392fd2a94557f4576cf5a6f94630dd55092
Alain Martin <amartin@keytrol.org>
2015-09-11 20:18:12 +0200

    Introduce KL::KeyMapping

---
+++ include/KL/Keyboard/KeyMapping.hpp
@@ -0,0 +1,39 @@
+// KeytroL
+// Copyright (C) 2015 Alain Martin
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+
+#include "KL/Keyboard/Keyboard.hpp"
+
+#include <functional>
+#include <map>
+#include <utility>
+
+
+namespace KL
+{
+
+class KeyMapping
+{
+    using Code = Keyboard::KeyCode;
+    using State = Keyboard::KeyState;
+
+public:
+    std::function<void()> & at(Code keyCode, State keyState);
+    const std::function<void()> & at(Code keyCode, State keyState) const;
+
+private:
+    std::map<std::pair<Code, State>, std::function<void()>> mKeyToFunction;
+};
+
+} // namespace KL
